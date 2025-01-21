7eaa0ac6b7e5eadf3d9eaa398c7409edb6be8dcf
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-20 23:31:52 -0700

    added some font styles

--- src/components/TopNavigation/Burger.jsx
+++ src/components/TopNavigation/Burger.jsx
@@ -5,9 +5,14 @@ export default function Burger() {
   document.body.style.backgroundColor = "black";
   return (
     <div>
-      <div className="flex">
-        <img className=" h-10 w-10" src={toogleImage} alt="" />
-        <img className=" h-10 w-10" src={YoutubeLogo} alt="" />
+      <div className="flex gap-4 mt-4 text-white">
+        <div className="border-2 p-1 h-8 rounded border-gray-700">
+          <img className="  align-middle h-5 w-5" src={toogleImage} alt="" />
+        </div>
+        <div>
+          <img className=" h-10 w-10" src={YoutubeLogo} alt="" />
+        </div>
+        <div>DashBoard</div>
       </div>
     </div>
   );
