a98a975a7a09e5152afae569578e98130fa149c2
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-20 23:50:15 -0700

    Harmburger menu don

--- src/components/TopNavigation/Burger.jsx
+++ src/components/TopNavigation/Burger.jsx
@@ -5,14 +5,14 @@ export default function Burger() {
   document.body.style.backgroundColor = "black";
   return (
     <div>
-      <div className="flex gap-4 mt-4 text-white">
+      <div className="flex gap-4 mt-4 text-white ml-4 ">
         <div className="border-2 p-1 h-8 rounded border-gray-700">
           <img className="  align-middle h-5 w-5" src={toogleImage} alt="" />
         </div>
         <div>
-          <img className=" h-10 w-10" src={YoutubeLogo} alt="" />
+          <img className=" h-8 w-8" src={YoutubeLogo} alt="" />
         </div>
-        <div>DashBoard</div>
+        <div className="text-sm font-medium text-gray-200 mt-2">Dashboard</div>
       </div>
     </div>
   );
--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -5,7 +5,7 @@ import Search from "./Serach";
 export default function TopNaviagtion() {
   return (
     <div>
-      <div className="flex justify-between">
+      <div className="flex justify-between border-b-gray-700 bg-Robin h-16 border-b-2">
         <Burger></Burger>
         <Search></Search>
         <Profile></Profile>
