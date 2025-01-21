5bd808273ac43c8b9d17e36143678b2c06287beb
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-20 23:54:33 -0700

    Working on the right side menu

--- src/components/TopNavigation/Profile.jsx
+++ src/components/TopNavigation/Profile.jsx
@@ -1,3 +1,12 @@
 export default function Profile() {
-  return <div> Profile</div>;
+  return (
+    <div className="text-white">
+      <div className="flex gap-3">
+        <div>asa</div>
+        <div>asa</div>
+        <div>asa</div>
+        <div>asa</div>
+      </div>
+    </div>
+  );
 }
