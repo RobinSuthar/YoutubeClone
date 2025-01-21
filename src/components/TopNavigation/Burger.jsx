07a9548a8a3016d2625f371904dd33870fb6cf6e
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-20 23:23:32 -0700

    added logo

--- src/components/TopNavigation/Burger.jsx
+++ src/components/TopNavigation/Burger.jsx
@@ -1,13 +1,13 @@
-import toogleImage from "../../Images/menu.png";
-import YoutubeLogo from "../../Images/unnamed.jpg";
+import toogleImage from "../../Images/menu (4).png";
+import YoutubeLogo from "../../Images/github-mark-white.png";
 
 export default function Burger() {
+  document.body.style.backgroundColor = "black";
   return (
     <div>
-      <div>
-        <img src={toogleImage} alt="" />
-
-        <img src={YoutubeLogo} alt="" />
+      <div className="flex">
+        <img className=" h-10 w-10" src={toogleImage} alt="" />
+        <img className=" h-10 w-10" src={YoutubeLogo} alt="" />
       </div>
     </div>
   );
